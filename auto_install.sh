f7403384b7a2b45ed8112d864a998a8192bba3c6
lescientifik <theophraste.henry@gmail.com>
2017-08-20 12:21:32 +0200

    Update auto_install.sh
    add gdebi for chrome and r studio install
    remove spyder (not working)

--- auto_install.sh
+++ auto_install.sh
@@ -1,16 +1,16 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
-sudo apt-get update -y --force-yes
-sudo apt-get upgrade -y --force-yes
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://download1.rstudio.org/rstudio-xenial-1.0.153-amd64.deb
-sudo apt-get install -y --force-yes r-base r-base-dev \
+sudo apt-get update -y
+sudo apt-get upgrade -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+wget https://download1.rstudio.org/rstudio-xenial-1.0.153-amd64.deb -O rstudio.deb
+sudo apt-get install -y r-base r-base-dev \
                      python3-pip python3-venv\
                      build-essential libssl-dev libffi-dev python-dev \
                      vlc
-sudo apt install -y --force-yes google-chrome-stable_current_amd64.deb \
-                     rstudio-xenial-1.0.153-amd64.deb \
+sudo apt install -y gdebi
+sudo gdebi -n rstudio.deb \
+                     chrome.deb \
 pip3 install --upgrade pip
-pip3 install spyder
 
 
